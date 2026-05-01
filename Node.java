f89f3e46f7e615313fde14ddaa4c3c5f41b3f97d
VincEli0 <94383011+VincEli0@users.noreply.github.com>
2026-05-01 05:32:30 +0000

    Add files via upload

--- Node.java
+++ Node.java
@@ -166,7 +166,7 @@ public class Node implements NodeInterface {
         this.socket = new DatagramSocket(portNumber);
 
         if (nodeName!= null){
-            addressBook.put(nodeName, "10.216.34.172:" + portNumber);//this is just for local testing remember to use the actual ipaddress later on
+            addressBook.put(nodeName, InetAddress.getLocalHost().getHostAddress() + portNumber);
         }
 
         List<String> bootstrapAddresses = List.of( //this is because nodes are dead
